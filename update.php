8cfc28fb7d4de3c4fb93cd1150966820ec751a12
=Christian Titus Fernanda <christiantitus.2020@student.uny.ac.id>
2022-12-23 11:19:46 +0700

    Anu

--- update.php
+++ update.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To Do List Kehidupan</title>
+    <title>Udate</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
-<body class="bg-primary">
+<?php
+$id = $_GET['ID'];
+include "config.php";
+$Rdata = mysqli_query($con, "select * from tbltodolistkehidupan where ID = $id");
+
+
+?>
+
+<body class="bg-info">
     <form action="insert.php" method="POST">
         <div class="container">
             <div class="row justify-content-center m-auto shadow bg-white mt-3 py-3 col-md-6">
-                <h3 class="text-center"><b>TO DO LIST KEHIDUPAN</b></h3>
+                <h3 class="text-center"><b>Update To Do List Kehidupan</b></h3>
             <div class="col-8">
                 <input type="text" name="" class="form-control">
             </div>
@@ -24,33 +31,4 @@
         </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </form>
-
-<!--getdata-->
-<?php
-include "config.php";
-$rawData = mysqli_query($con, "select * from tbltodolistkehidupan");
-
-?>
-<div class= "container">
-    <div class="col-8 bg-white m-auto mt-3">
-
-        <table class="table">
-            <tbody>
-                <?php
-                while ($row = mysqli_fetch_array($rawData)) {
-                ?>
-                <tr>
-                    <td><?php echo $row['ID'] ?></td>
-                    <td><?php echo $row['List'] ?></td>
-                    <td style="width: 10%;"><a href="delete.php? ID= <?php echo $row['ID'] ?>" class="btn btn-outline-danger">Hapus</td>
-                    <td style="width: 10%;"><a href="update.php? ID= <?php echo $row['ID'] ?>" class="btn btn-outline-success">Update</td>
-                </tr>
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
-</div>
-</body>
 </html>
